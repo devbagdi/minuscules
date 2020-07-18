add81ef328d8619d9853153e8b2dabf9eeb65a58
ankush kaushik <ankushkauhsik123@gmail.com>
2020-07-18 12:39:22 +0530

    in between of gallery

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('#mycarousel').carousel({interval:1000});
     $('#carouselButton').click(function(){
         if ($('#carouselButton').children('span').hasClass('fa-pause')){
-            $('#mycarousel').carousel('pause');  
+            $('#mycarousels').carousel('pause');  
             $('#carouselButton').children('span').removeClass('fa-pause');
             $('#carouselButton').children('span').addClass('fa-play');
         }
@@ -12,4 +12,28 @@ $(document).ready(function(){
             $('#carouselButton').children('span').addClass('fa-pause');
         }
     });
+});
+
+   
+
+
+$('.gallery-modal').click(function() {
+
+    var modaltitle = $(this).attr('data-title');
+    $('#modal-title').html(modaltitle);
+    var modalImageSource = $(this).attr('data-imageSource');
+    document.getElementById('modal-image').setAttribute("src",modalImageSource);
+    var modalPicCredit = $(this).attr('data-picCredit');
+    $('#name').html(modalPicCredit);
+    var modalPicLocation = $(this).attr('data-picLocation');
+    $('#location').html(modalPicLocation);
+    var modalPicCaption = $(this).attr('data-picCaption');
+    $('#modal-caption').html(modalPicCaption);
+    var modalInstaHref = $(this).attr('data-instaHref');
+    $("#instaHref").attr("href", modalInstaHref);
+    var modalFacebookHref = $(this).attr('data-facebookHref');
+    $("#facebookHref").attr("href", modalFacebookHref);
+    var modalTwitterHref = $(this).attr('data-twitterHref');
+    $("#twitterHref").attr("href", modalTwitterHref);
+    $('#gallerymodal').modal();
 });
