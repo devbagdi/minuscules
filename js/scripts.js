d2d1d1632d1c5043ba18224bc41fbfa824987d0e
ankush kaushik <ankushkauhsik123@gmail.com>
2020-08-26 17:36:06 +0530

    newsfeedcolumn str

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,32 @@ $('#myList a').on('click', function (e) {
     $(this).tab('show')
 })
 
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  document.getElementById(cityName).classList.add("active");
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").onmouseover();
+
+
    
 
 
